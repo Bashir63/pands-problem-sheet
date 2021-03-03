b506276d25d7a6848c6f7bd9dfec20ba9a8814f6
Bashir <nilsazi089@gmail.com>
2021-03-03 00:24:27 +0000

    squareroot.py

--- Week06/squareroot.py
+++ Week06/squareroot.py
@@ -5,20 +5,26 @@ fromUser = float(input("Please enter a positive number :"))
 # Asking user for an input to find out the square root of
 # We are type casting it for the float value
 
-howMany = int (input("How many times need to calculate the approx :"))
-""" We are also asking the user for another input to let us know 
-for how many times to iterate in this method. This time it will be 
-integer number 
-"""
+# Start of the function (called sqrt)
+def sqrt(fromUser):
 
-approx = 0.5*fromUser
-#approx is a variable here
+    #approx = the first number to test (= half the input number ( or half 14.5))
+    approx = 0.5*fromUser
 
-for i in range(howMany):
-    betterapprox = 0.5*(approx + fromUser/approx)
-    # betterapprox is an another variable to hold the final result of the equation
+   # Start the loop for 20 times
+    for i in range(20):
+        
+        #Newton's formula
+        betterapprox = 0.5*(approx + fromUser/approx)
     
-    approx = betterapprox
+        #defining the new number to test for the next iteration
+        approx = betterapprox
 
+    # End the loop
+    return betterapprox    
 
-print ("The square root of 14.5 is approx. ",betterapprox)    
+#best approx = the process of the function(sqrt)
+bestapprox = (sqrt(fromUser))
+
+#print the result of the function defined on the line above rounded to 1 decimal place 
+print ("The square root of {} is approx. ".format(fromUser),round(bestapprox,1))    
