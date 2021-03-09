2eae7e6c532862555e27dcc63869f6a8c0d1218f
Bashir <nilsazi089@gmail.com>
2021-03-09 14:19:17 +0000

    week05

--- Week05/weekday.py
+++ Week05/weekday.py
@@ -1,9 +1,21 @@
-# Write a program that outputs whether or not today is a weekday.
-# Author : Bashir Ahammed
+# Writing a program that outputs whether or not today is a weekday.
+# The program allows to search the web to find out what day it is
 
-from dateime import dateime
-a = datetime.today(). strftime('%A')
+
+# Author : Bashir Ahammed
+# Student ID : G00268666
+
+# For this program I'll have to import the datetime from the python library to find out what day it is
+# Then I'll have to use the if/else statement to find out if it's a weekday or a weekend
+
+# Importing datetime from the python library
+from datetime import datetime
+a = datetime.today().strftime('%A')
+
+# In here if statement shows all the weekdays and it'll print accordingly if it's a week day and if it is not a week day it's a weekened where it will print by the else part of the statement
 if a == "Monday" or a=="Tuesday" or a=="Wednesday" or a=="Thursday" or a=="Friday":
+    # Here is a print method to check if that is a weekday 
     print("Yes, Unfortunately today is a weekday.")
 else:
-    print("It is the weekend, Yay")    
+    # Here is a Print method to find out if it is a weekened today
+    print("It is the weekend, Yay")  
