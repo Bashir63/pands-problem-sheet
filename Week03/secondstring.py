876edb69d86a7322548cfa113cba4ac3c78592d5
Bashir <nilsazi089@gmail.com>
2021-03-09 12:22:21 +0000

    week03

--- Week03/secondstring.py
+++ Week03/secondstring.py
@@ -1,9 +1,13 @@
 
-# A string in every second letter in reverse order
+# Writing a program that takes asks a user to input a string and outputs every second letter in revese order
 # Author: Bashir Ahammed
+# Student ID: G00268666
 
-
+# Asking the user for an input which will be an integer
 sentenceInput = (input ("Please enter a sentence: "))
-sliceValue = slice(sentenceInput[::-2])
 
-print('{}'.format(sliceValue))
+# Method to reverse the every second letter of the user's string
+sentencevalue = sentenceInput[::-2]
+
+# Finally using printing method to display the result of the program
+print(sentencevalue)
