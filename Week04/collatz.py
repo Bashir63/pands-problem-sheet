8aa0cc510ae082d4366284a4e1cce14a2377c8c3
Bashir <nilsazi089@gmail.com>
2021-03-14 14:34:51 +0000

    week04

--- Week04/collatz.py
+++ Week04/collatz.py
@@ -12,13 +12,14 @@
 #Taking input from user
 n = int( input("Please input a positive integer "))
 
-# Using the while loop to check if the input (n) is equelty one 
+# Using the while loop to check if the input (n) is not equelty one 
 while n != 1:
     # Using the if statement to check the number even or odd by dividing by two
     if n % 2 == 0:
         n = n/2
-    # Using the else statement if the number is odd which will be multified by three and added one 
+    # Using the else statement if the number is odd which will be multiplied by three and added one 
     else:
         n = 3*n+1
-    # Finally using the round method to get the integer results and printing the results
+    
+    # Finally using the round method and printing the results
     print (round(n)) # n is the input here 
