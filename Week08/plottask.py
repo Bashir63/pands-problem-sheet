b10ef889629261d5373c392afc675535a69b6769
Bashir <nilsazi089@gmail.com>
2021-03-26 20:23:35 +0000

    Week08

---
+++ Week08/plottask.py
@@ -0,0 +1,27 @@
+# Writing a program which displays a plot of the functions f(x)=x, g(x)=x*x and h(x)=x*x*x in the range [0, 4] on the one set axes
+
+# Student : Bashir Ahammed
+# Student ID : G00268666
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create the vectors X and Y
+x = np.array(range(0,4))
+f(x) = x 
+
+a = np.array(range(0,4))
+g(x) = x*x
+
+c = np.array(range(0,4))
+h(x) = x*x*x
+
+
+# Create the plot
+plt.plot(x,y)
+plt.plot(a,b)
+plt.plot(c,d)
+
+# Show the plot
+plt.show()
